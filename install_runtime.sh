41b2fac980253a77041f67f2f4611baa0c9ba799
spoonofpower <spoonofpower@gmail.com>
2017-06-22 23:48:33 -0700

    Upload build

---
+++ install_runtime.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ex
+
+if [[ -f $HOME/tools/bin/runtime ]]; then
+    # Already installed
+    exit 0
+fi
+
+curl -o $HOME/tools/bin/runtime https://s3-us-west-2.amazonaws.com/runtime-dev-download/linux/runtime
+chmod a+x $HOME/tools/bin/runtime
