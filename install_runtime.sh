362d3a5cae60348dbf45731ddf98eeb502374b2a
spoonofpower <spoonofpower@gmail.com>
2017-07-07 14:53:02 -0700

    newt is cached now

--- install_runtime.sh
+++ install_runtime.sh
@@ -3,9 +3,6 @@
 curl -o $HOME/tools/bin/rt_runner https://s3-us-west-2.amazonaws.com/runtime-dev-download/linux/rt_runner
 chmod a+x $HOME/tools/bin/rt_runner
 
-go get mynewt.apache.org/newt/newt
-mv $GOPATH/bin/newt $HOME/tools/bin/newt
-
 if [[ -f $HOME/tools/bin/runtime ]]; then
     # Already installed
     exit 0
@@ -16,3 +13,5 @@ chmod a+x $HOME/tools/bin/runtime
 curl -o $HOME/tools/bin/runtime-ci https://s3-us-west-2.amazonaws.com/runtime-dev-download/linux/runtime-ci
 chmod a+x $HOME/tools/bin/runtime-ci
 
+go get mynewt.apache.org/newt/newt
+mv $GOPATH/bin/newt $HOME/tools/bin/newt
