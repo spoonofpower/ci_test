31b98f960f017649a0cfb4becad334f9e0e912ab
spoonofpower <spoonofpower@gmail.com>
2017-07-07 13:57:55 -0700

    Install more runtime tools
    We'll probably be iterating over rt_runner changes, so always update it.

--- install_runtime.sh
+++ install_runtime.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -ex
 
+curl -o $HOME/tools/bin/rt_runner https://s3-us-west-2.amazonaws.com/runtime-dev-download/linux/rt_runner
+chmod a+x $HOME/tools/bin/rt_runner
+
 if [[ -f $HOME/tools/bin/runtime ]]; then
     # Already installed
     exit 0
@@ -7,3 +10,5 @@ fi
 
 curl -o $HOME/tools/bin/runtime https://s3-us-west-2.amazonaws.com/runtime-dev-download/linux/runtime
 chmod a+x $HOME/tools/bin/runtime
+curl -o $HOME/tools/bin/runtime https://s3-us-west-2.amazonaws.com/runtime-dev-download/linux/runtime-ci
+chmod a+x $HOME/tools/bin/runtime-ci
